3257c3838f8ff64854b48a7c3135fa1ee215a211
syeda zaidi <zaidisyeda03@gmail.com>
2019-08-27 13:32:13 -0700

    displaying items for sale

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,28 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon",
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId +"\n");
+    displayItems();
+});
+
+displayItems = () => {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        console.log("\n ----- Items for Sale ----- \n");
+        for (i=0; i<res.length; i++) {
+            console.log("item-id : " + res[i].item_id + "\n"+ " product : " + res[i].product_name + " Price : $" + res[i].price + "\n");
+        }
+        connection.end();
+    })
+};
--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products(
-    item_id NOT NULL AUTO_INCREMENT,
+    item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(100),
     price DECIMAL(10,2),
@@ -16,8 +16,8 @@ CREATE TABLE products(
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES  ("Chiquita Bananas", "Produce", 0.20, 400),
         ("Golden Apples", "Produce", 0.40, 500),
-        ("Dove Conditioner", "Grocery", 6.25, 6.27)
-        ("Tropicana Orange Juice", "Grocery", 4.45, 267)
+        ("Dove Conditioner", "Grocery", 6.25, 6.27),
+        ("Tropicana Orange Juice", "Grocery", 4.45, 267),
         ('Horizon Organic Milk', 'Grocery', 4.50, 200),
         ("Ben & Jerry Ice Cream", "Grocery", 4.45, 432),
         ("Paper Towels", "Grocery", 4.25, 400),
@@ -25,4 +25,3 @@ VALUES  ("Chiquita Bananas", "Produce", 0.20, 400),
         ("Band Aid", "Pharmacy", 3.25, 550),
         ("T-shirts", "Clothing", 7, 250);
 
-        
