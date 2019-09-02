0d3f669f9c311dca3ad28729de7a5e15cecbbaef
syeda zaidi <zaidisyeda03@gmail.com>
2019-09-02 01:32:17 -0700

    adding inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -30,9 +30,12 @@ menuPrompt = () => {
         } else if (action.menu === "View Low Inventory") {
             displayLowInventory();
             connection.end();
+        } else if (action.menu === "Add to Inventory") {
+            addToInventory();
+            // connection.end();
         }
     })
-}
+};
 
 function displayItems() {
     connection.query("SELECT * FROM products", function (err, res) {
@@ -43,7 +46,7 @@ function displayItems() {
             console.log("Item-id : " + res[i].item_id + "\nProduct : " + res[i].product_name + "\nPrice : $" + res[i].price + "\nStock Quantity : " + res[i].stock_quantity + "\n-----------------\n");
         }
     })
-}
+};
 
 function displayLowInventory() {
     connection.query("SELECT * FROM products Where stock_quantity < 100", function (err, res) {
@@ -54,4 +57,55 @@ function displayLowInventory() {
             console.log("Item-id : " + res[i].item_id + "\nProduct : " + res[i].product_name + "\nPrice : $" + res[i].price + "\nStock Quantity : " + res[i].stock_quantity + "\n-----------------\n");
         }
     })
-}
+};
+
+function addToInventory() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the Item-ID of the product you want to add inventory in",
+            name: "ItemToAdd",
+            validate: (value) => {
+                if (!isNaN(value) && value > 0 && value < 11) {
+                    return true;
+                } else {
+                    return false;
+                };
+            },
+        },
+        {
+            type: "input",
+            message: "Enter the quantity you want to add to stock",
+            name: "QuantityToAdd",
+            validate: (value) => {
+                if (isNaN(value)) {
+                    return false;
+                } else {
+                    return true;
+                };
+            },
+        }
+    ]).then(function (answer) {
+        connection.query("SELECT * FROM products WHERE ?",
+            {
+                item_id: answer.ItemToAdd,
+            }, function (err, res) {
+                if (err) throw err;
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: (res[0].stock_quantity + parseInt(answer.QuantityToAdd)),
+                        },
+                        {
+                            item_id: answer.ItemToAdd,
+                        }],
+                    function (err, response) {
+                        if (err) throw err;
+
+                        console.log("\nInventory Updated " + response.affectedRows + " row affected\n");
+                        connection.end();
+                    },
+                );
+            });
+    });
+};
